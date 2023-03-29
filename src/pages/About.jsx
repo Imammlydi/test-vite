82913723cf90c9e25cdb469323208941c884ab57
Imam Mulyadi <imammlydi892@gmail.com>
2023-03-30 05:55:11 +0700

    add layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import Case from "../components/Case";
 export default function About() {
     return (
-        // <Case>
-            <div className="mx-auto lg:max-w-7xl md:items-center md:px-8 px-4 ">
-                <h4 className="text-2xl">I'm About Page</h4>
-                <p className="text-lg leading-relaxed text-gray-400">
-                    You can go to this page because the React router.
-                </p>
-            </div>
-        // </Case>
+        <>
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 lg:gap-1 mx-auto lg:max-w-7xl md:px-8 px-4">
+            <div className="bg-primary flex justify-center py-3"><h4 className="text-2xl">I'm About Page</h4></div>
+            <div className="bg-primary flex justify-center py-3"><h4 className="text-2xl">I'm About Page</h4></div>
+            <div className="bg-primary flex justify-center py-3"><h4 className="text-2xl">I'm About Page</h4></div>
+            <div className="bg-primary flex justify-center py-3"><h4 className="text-2xl">I'm About Page</h4></div>
+            <div className="bg-primary flex justify-center py-3"><h4 className="text-2xl">I'm About Page</h4></div>
+            <div className="bg-primary flex justify-center py-3"><h4 className="text-2xl">I'm About Page</h4></div>
+          
+         </div>
+      
+            {/* // <div className="mx-auto lg:max-w-7xl md:items-center md:px-8 px-4 ">
+            //     <h4 className="text-2xl">I'm About Page</h4>
+            //     <p className="text-lg leading-relaxed text-gray-400">
+            //         You can go to this page because the React router.
+            //     </p>
+            // </div> */}
+        </>
     );
 }
