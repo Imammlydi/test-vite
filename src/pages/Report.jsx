f5a28b0da54623a0c179d1323cef736b2e63bdc4
Imam Mulyadi <imammlydi892@gmail.com>
2023-04-11 05:51:09 +0700

    modif  report page

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -4,37 +4,34 @@ import { CardItem1, CardItem2, CardItem3 } from "../components";
 export default function Report() {
     const Search = () => {
         return (
-            <form class="mb-4    pt-1 pb-2 ">
-                <div class="mb-4">
+            <form class="mb-4    pt-1 ">
+                <div class="mb-1">
                     <input
-                        className="border-1 text-slate-50 focus:ring-sky-500 mb-3 w-full appearance-none   rounded border border-graydisable     py-2 px-3 font-Bitter leading-tight  focus:border-primary focus:outline-none focus:ring-1 "
+                        className="border-1 text-slate-50 focus:ring-sky-500  appearance-none rounded   border border-graydisable py-3     px-3 font-Bitter leading-tight focus:border-primary  focus:outline-none focus:ring-1 lg:max-w-5xl "
                         id="search"
                         type="text"
                         placeholder="search"
                     />
+                    <button
+                        class="focus:shadow-outline ml-2 rounded bg-white py-2 px-4 font-Bitter font-bold text-white hover:bg-textPrimary  focus:outline-none"
+                        type="button"
+                    >
+                        search
+                    </button>
                 </div>
-                {/* <div class="flex items-center justify-between">
-                        <button
-                            class="focus:shadow-outline rounded bg-primary py-2 px-4 font-Bitter font-bold text-white hover:bg-textPrimary  focus:outline-none"
-                            type="button"
-                        >
-                            Sign In
-                        </button>
-                        <a
-                            class="text-blue-500 hover:text-blue-800 inline-block align-baseline font-Bitter text-sm font-bold"
-                            href="#"
-                        >
-                            Forgot Password?
-                        </a>
-                    </div> */}
             </form>
         );
     };
 
     return (
         <>
-            <div className="mx-auto grid grid-cols-1 gap-4 px-4 pt-8 md:grid-cols-2 md:px-8 lg:max-w-7xl lg:grid-cols-2 lg:gap-1">
+            <div className="mx-auto grid grid-cols-1 gap-4 px-4 pt-8 md:grid-cols-2 md:px-8 lg:max-w-7xl lg:grid-cols-2 lg:gap-1 ">
                 <Search />
+            </div>
+            <div className="mx-auto mb-3 flex gap-4 px-4 pt-8 md:px-8 lg:max-w-7xl ">
+                <h5 className="text-textPrimary">Category1</h5>
+                <h5 className="text-textPrimary">Category1</h5>
+                <h5 className="text-textPrimary">Category1</h5>
             </div>
             <div class="mx-auto grid grid-cols-1 gap-4 px-4 pt-1 md:grid-cols-2 md:px-8 lg:max-w-7xl lg:grid-cols-4 lg:gap-1">
                 <CardItem2 />
